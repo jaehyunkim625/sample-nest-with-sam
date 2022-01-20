acf6d025db3dc7a3e065db19c53436e0ba08d162
Chris <jaehyunkim625@gmail.com>
2022-01-21 01:58:42 +0900

    [MOD] change class name

--- src/user/domain/model/service/IUserFactory.ts
+++ src/user/domain/model/service/IUserFactory.ts
@@ -1,7 +1,7 @@
 import IUser from '../../interface/IUser';
 import CreateUserCommand from '../command/CreateUserCommand';
 
-export default abstract class IFactory {
+export default abstract class IUserFactory {
   protected abstract factoryMethod(createUserCommand: CreateUserCommand): IUser;
   public createUser(userCreateCommand: CreateUserCommand): IUser {
     const user = this.factoryMethod(userCreateCommand);
